9fa09de6c478dde3d43e7a6c5f4ba42d1366f16f
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-10 00:02:59 +0530

    CHG | Hero Section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+const CustomButton = () => {
+  return (
+    <button
+    disabled={false}
+    type={"button"}
+    className={`custom-btn`}
+    onClick={()=>{}}
+    >
+      <span className={`flex-1`}>
+        title
+      </span>
+    </button>
+  )
+}
+
+export default CustomButton

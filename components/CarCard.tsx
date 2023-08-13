6814e7452b85e2514afcf8e45e2b2556269600ae
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-13 23:55:54 +0530

    CHG | Car Catalouge I

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { CarProps } from '@/types';
 import { calculateCarRent } from '@/utils';
+import Image from 'next/image';
+import { CustomButton } from '.';
 
 
 interface CarCardProps {
@@ -19,9 +21,66 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
       <p className='flex mt-6 text-[32px] font-extrabold'>
         <span className='self-start text-[14px] font-semibold'>
-          {carRent}
+          $
+        </span>
+        {carRent}
+        <span className='self-end text-[14px] font-medium'>
+          /day
         </span>
       </p>
+
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image
+          src="/hero.png"
+          // width={50}
+          // height={50}
+          alt='car model'
+          fill
+          priority
+          className='object-contain'
+        />
+      </div>
+
+      <div className='relative flex w-full mt-2'>
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image 
+            src="/steering-wheel.svg"
+            width={20}
+            height={20}
+            alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? 'Automatic' : 'Manual'}
+            </p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image 
+            src="/tire.svg"
+            width={20}
+            height={20}
+            alt='tire'
+            />
+            <p className='text-[14px]'>
+              {drive.toUpperCase()}
+            </p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image 
+            src="/gas.svg"
+            width={20}
+            height={20}
+            alt='gas'
+            />
+            <p className='text-[14px]'>
+             {city_mpg} MPG
+            </p>
+          </div>
+        </div>
+        <div className='car-card__btn-container'>
+
+        </div>
+      </div>
     </div>
   )
 }

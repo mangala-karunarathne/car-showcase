bc9ae0df510ce9a4fb8f8fc3d0ffd30d3af6cec8
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-18 21:52:13 +0530

    CHG | Car Images

--- utils/index.ts
+++ utils/index.ts
@@ -37,4 +37,11 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const { make, year, model } = car;
 
   url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(' ')[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`
 };

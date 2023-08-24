3ebf788a1d99ca7b89402573675387ad51b30cc2
mangala-karunarathne <mangalaicc@gmail.com>
2023-08-24 07:40:37 +0530

    CHG | Custom Filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client"
 
-const CustomFilter = () => {
+import { CustomFilterProps } from "@/types"
+import { Listbox } from "@headlessui/react"
+import { useState } from "react"
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0])
   return (
-    <div>CustomFilter</div>
+    <div className="w-fit">
+      <Listbox
+      value={selected}
+      onChange={(e)=> setSelected(e)}
+      >
+        <div className="relative w-fit z-10">
+          <Listbox.Button className="custom-bilter__butn">
+            <span>Filter</span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
